ed7c2ff98d0e922484e3fa3e7089508395884910
Marcelo <romemarce1@gmail.com>
2021-02-14 16:10:13 -0300

    Update hero list and hero screen

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroCard } from './HeroCard';
 
 export const HeroList = (publisher) => {
 
-  const heroes = getHeroesByPublisher(publisher);
+  // guarda en memoria los heroes extraidos. Solo volvera a llamar la funcion si el publisher cambia
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  // const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <div className="row">
-      {heroes.map(hero => (
-        <HeroCard
-          key={hero.id}
-          {...hero} 
-        />
+    <div className="row animate__animated animate__fadeIn">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
       ))}
     </div>
   );
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Redirect, useParams } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
-  const {heroeId} = useParams()
-  // console.log(heroeId);
+export const HeroScreen = ({history}) => {
+  const { heroeId } = useParams();
 
-  const hero = getHeroById(heroeId)
+  // guarda en memoria los heroe extraido. Solo volvera a llamar la funcion si el id cambia
+  const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+  // const hero = getHeroById(heroeId);
 
-  if(!hero){
-    return <Redirect to="/" />
+  if (!hero) {
+    return <Redirect to="/" />;
   }
 
   const {
@@ -19,9 +20,47 @@ export const HeroScreen = () => {
     first_appearance,
     characters,
   } = hero;
+
+  const handleClick = () => {
+    if (history.length <= 2) {
+      return history.push("/");
+    } else {
+      return history.goBack();
+    }
+  };
+
   return (
-    <>
-      <h1>HeroScreen</h1>
-    </>
+    <div className="row m-2">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroeId}.jpg`}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+          alt={superhero}
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First apperance: </b>
+            {first_appearance}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button onClick={handleClick} className="btn btn-dark">
+          Return
+        </button>
+      </div>
+    </div>
   );
 }
